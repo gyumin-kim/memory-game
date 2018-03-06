2c47c7cec3e084bd678f66e596df0b4b2f8972ef
Gyumin Kim <starkying@gmail.com>
2018-03-06 16:51:25 +0900

    Add comments on timer.

--- js/app.js
+++ js/app.js
@@ -195,15 +195,15 @@ restart.addEventListener('click', function() {
     for (let i = 0; i < shuffledCards.length; i++)
         deck.appendChild(shuffledCards[i]);
     resetStar();
+    // Initialize timer
+    timeDisplay.textContent = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
 });
 
 let timeDisplay = document.querySelector('.timer');
 let seconds = 0, minutes = 0, hours = 0;
 let t;
-restart.onclick = function() {
-    timeDisplay.textContent = "00:00:00";
-    seconds = 0; minutes = 0; hours = 0;
-}
+
 function add() {
     seconds++;
     if (seconds >= 60) {
@@ -215,16 +215,18 @@ function add() {
         }
     }
     
+    // Display current time in two digits.
     timeDisplay.textContent = 
     (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" 
     + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" 
     + (seconds > 9 ? seconds : "0" + seconds);
 
-    timer();
+    timer();    // Update timer every second 
 }
 
+// Call add() every second and save it to t
 function timer() {
-    t = setTimeout(add, 1000);	// 1초마다 add해서 t에 반영
+    t = setTimeout(add, 1000);
 }
 
-timer();
+timer();    // Turn on timer when you open the game
